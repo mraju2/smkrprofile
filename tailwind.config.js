862196cf5e39138b7a2e43ee108a17e8bd1fe830
manikantaraju somala <manikantarajusomala@manikantarajus-MacBook-Pro.local>
2022-12-10 17:48:33 +0530

    [4-theme-setup] Add theme colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,30 @@
 "use strict";
 
+const colors = {
+  white: "#fffff",
+  black: "#00000",
+  green: { 1: "#64FFDA" },
+  blue: { 1: "#112240", 2: "#1C2E43" },
+  gray: { 1: "#CCD6F6", 2: "#A8B2D1" },
+};
 module.exports = {
   content: ["src/**/*.tsx"],
   theme: {
     extend: {},
     colors: {
+      ...colors,
       transparent: "transparent",
-      current: "currentColor",
-      primary: "#050505",
+      "background-1": colors.blue[1],
       secondary: "#2a2b2b",
       white: "#ffffff",
-      purple: "#3f3cbb",
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
       silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
+      "text-1": colors.gray[1],
+      "text-2": colors.gray[2],
+      "text-green": colors.green[1],
+    },
+    fontFamily: {
+      sans: ["Inter", "Calibre", "San Francisco", "SF Pro Text"],
+      mono: ["SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", "monospace"],
     },
   },
   plugins: [],
